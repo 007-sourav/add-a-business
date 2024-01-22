08c01e02e78b8c5261c5973a8fedcb2f1e04140e
Sourav Rathod <souravrathod35@gmail.com>
2024-01-22 10:34:52 +0530

    task has been completed

--- script.js
+++ script.js
@@ -5,25 +5,26 @@ var ist = document.querySelector("h5");
 
 
 
-var addbusinesss =document.querySelector("#add");
+var btn =document.querySelector("#add");
 // #add for selecting button's id
 
 var check=0;
 
 
-addbusinesss.addEventListener("click",function(){
+btn.addEventListener("click",function(){
     
     if(check ==0){
         ist.innerHTML="Business Partner";
         ist.style.color="green";
         console.log("hi hi business");
-
+        btn.innerHTML="remove business";
         check=1;
         return;
     }else{
         ist.innerHTML="not-business";
         ist.style.color="red";
         console.log("not business");
+        btn.innerHTML="add business";
         check=0;
         return;
     }
